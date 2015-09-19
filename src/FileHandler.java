2c5b970420a1382df2dbdd6651d44e97a49e4d76
A0124402 <A0124402@u.nus.edu>
2015-09-19 20:49:02 +0800

    FileHandler project and events methods working

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -25,21 +23,27 @@ import java.util.ArrayList;
 public class FileHandler implements FileManager{
 	private static final String EVENT_OVERVIEWER = "overviewer.txt";
 	private File inputFile;
-	private ArrayList<String> availableMonths;
+	private String eventPath;
+	private String todoPath;
+	private String projectPath;
+	
+	private FileProjectHandler fProjH;
+	private FileEventHandler fEventH;
+	
 	
 /************************************* Public Methods ****************************************************/
 	public FileHandler(){
+		new StartUpHandler();
 		readOverviewerFile();
+		
+		fEventH = new FileEventHandler(eventPath);
+		fProjH = new FileProjectHandler(projectPath);
+		
 	}
 	
 	@Override
 	public ArrayList<Event> retrieveEventByDate(String date){
-		
-		ArrayList<Event> eventBookByMonth = retrieveEventByMonth(date);
-		
-		ArrayList<Event> eventBookByDate = filterEventsToSpecificDate(date, eventBookByMonth);
-		
-		return eventBookByDate;
+		return fEventH.retrieveEventByDate(date);
 	}
 	
 	@Override
@@ -56,46 +60,12 @@ public class FileHandler implements FileManager{
 
 	@Override
 	public ArrayList<Event> retrieveProjectTimeLine(String projectName) {
-		// TODO Auto-generated method stub
-		return null;
+		return fProjH.retrieveProject(projectName);
 	}
 	
 	@Override
 	public boolean saveNewEventHandler(Event event){
-
-		boolean dateWillSpillOverMonth = false;
-		
-		String startDate = event.getStartDateString();
-		String fileName = setFileName(startDate);
-		if(!availableMonths.contains(fileName)){
-			createNewMonthFile(fileName);
-			updateOverviewFile(fileName);
-			return false;
-		}
-		
-		ArrayList<Event> eventBook = retrieveEventByMonth(startDate);
-		eventBook.add(event);
-		sortEventsByDate();
-		saveEventBook(setFileName(startDate), eventBook);
-		
-		String endDate = event.getEndDateString();
-		if(!startDate.equals(endDate)){
-			//TODO: spill over days
-			
-			if(!availableMonths.contains(setFileName(endDate)) ){
-				createNewMonthFile(setFileName(endDate));
-				dateWillSpillOverMonth = true;
-				updateOverviewFile(setFileName(endDate));
-			}
-		}
-		
-		if(dateWillSpillOverMonth){
-			eventBook = retrieveEventByMonth(endDate);
-			eventBook.add(event);
-			sortEventsByDate();
-			saveEventBook(setFileName(startDate), eventBook);
-		}
-		return true;
+		return fEventH.saveNewEventHandler(event);
 	}
 	
 	@Override
@@ -118,54 +88,17 @@ public class FileHandler implements FileManager{
 
 	@Override
 	public boolean createNewProject(String projectName) {
-		// TODO Auto-generated method stub
-		return false;
+		return fProjH.createNewProject(projectName);
 	}
 
 	@Override
 	public boolean saveEditedProjectDetails(ArrayList<Event> projectBook, String projectName) {
-		// TODO Auto-generated method stub
-		return false;
+		return fProjH.saveEditedProjectDetails(projectBook, projectName);
 	}
 
 	@Override
 	public ArrayList<String> getListOfExistingProject() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private ArrayList<Event> retrieveEventByMonth(String date){
-		
-		String fileName = setFileName(date);
-		selectFileAsInputFile(fileName);
-		
-		ArrayList<Event> eventBook  = new ArrayList<Event>();
-		String eventName, startDate, endDate, startTime, endTime, addInfo;
-		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-			String lineOfText;
-			
-			while( (lineOfText = reader.readLine()) != null ){
-				eventName = lineOfText;
-				addInfo = reader.readLine();
-				startDate = reader.readLine();
-				endDate = reader.readLine();
-				startTime = reader.readLine();
-				endTime = reader.readLine();			
-				
-				Event event = new Event(eventName, startDate, endDate, startTime, endTime, addInfo);
-				eventBook.add(event);
-			}
-			
-			reader.close();		 
-			return eventBook;
-		}
-		catch (FileNotFoundException e) {
-			return eventBook;
-		}catch (IOException e) {
-			return eventBook;
-		}	
+		return fProjH.getListOfExistingProjects();
 	}
 	
 	@Override
@@ -179,15 +112,14 @@ public class FileHandler implements FileManager{
 	
 	private void readOverviewerFile() {
 		inputFile = new File(EVENT_OVERVIEWER);	
-		availableMonths = new ArrayList<String>();
-		
+			
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-			String date;
 			
-			while((date = reader.readLine()) != (null)){
-				availableMonths.add(date);
-			}		
+			todoPath = reader.readLine();
+			eventPath = reader.readLine();
+			projectPath = reader.readLine();
+			
 			reader.close();		 
 		}
 		catch (FileNotFoundException e) {
@@ -197,88 +129,88 @@ public class FileHandler implements FileManager{
 		}
 	}
 	
-	private boolean updateOverviewFile(String fileName){
-		availableMonths.add(fileName);
-		try{
-			File outfile = new File(EVENT_OVERVIEWER);
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
-			
-			for(String month : availableMonths){
-				writer.write(month.trim());
-				writer.newLine();
-			}
-			writer.close();
-			return true;
-	
-		}catch(IOException e){
-			System.out.println("File cannot be written.\n");
-			return false;
-		}
-	}
+//	private boolean updateOverviewFile(String fileName){
+//		availableMonths.add(fileName);
+//		try{
+//			File outfile = new File(EVENT_OVERVIEWER);
+//			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
+//			
+//			for(String month : availableMonths){
+//				writer.write(month.trim());
+//				writer.newLine();
+//			}
+//			writer.close();
+//			return true;
+//	
+//		}catch(IOException e){
+//			System.out.println("File cannot be written.\n");
+//			return false;
+//		}
+//	}
 	
 /*********************************** File Manipulation *************************************************/	
 	
-	private String setFileName(String date){
-		String[] brokenUpDate = date.split(" ");
-		return brokenUpDate[1].concat(brokenUpDate[2] + ".txt");	
-	}
-
-	private void selectFileAsInputFile(String FileName){
-		inputFile = new File(FileName);
-	}
-	
-	private void createNewMonthFile(String textFileName){
-
-		try{
-			File newFile = new File(textFileName);
-			newFile.createNewFile();
-			
-		}catch(IOException e){
-			
-		}catch(SecurityException e){
-			
-		}
-	}
-	
-/************************************* Reading File ***************************************************/
-	
-	private ArrayList<Event> filterEventsToSpecificDate(String date, ArrayList<Event> eventBookByMonth) {
-		ArrayList<Event> eventBookByDate = new ArrayList<Event>();
-		
-		for(Event anEvent : eventBookByMonth){
-			if( anEvent.getStartDateString().toLowerCase().equals(date.toLowerCase())){
-				eventBookByDate.add(anEvent);
-			}
-		}
-		return eventBookByDate;
-	}
-	
-/********************************** Writing on File ***************************************************/
-	
-	private boolean saveEventBook(String textFileName, ArrayList<Event> eventBook){
-		
-		try{
-			File outfile = new File(textFileName);
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
-			
-			
-			
-			for(Event anEvent : eventBook){
-				writer.write(anEvent.toString()); 
-				writer.newLine();
-			}
-			writer.close();
-			return true;
-	
-		}catch(IOException e){
-			System.out.println("File cannot be written.\n");
-			return false;
-		}
-	}
-	
-	private void sortEventsByDate(){
-		
-	}
+//	private String setFileName(String date){
+//		String[] brokenUpDate = date.split(" ");
+//		return brokenUpDate[1].concat(brokenUpDate[2] + ".txt");	
+//	}
+//
+//	private void selectFileAsInputFile(String FileName){
+//		inputFile = new File(FileName);
+//	}
+//	
+//	private void createNewMonthFile(String textFileName){
+//
+//		try{
+//			File newFile = new File(textFileName);
+//			newFile.createNewFile();
+//			
+//		}catch(IOException e){
+//			
+//		}catch(SecurityException e){
+//			
+//		}
+//	}
+//	
+///************************************* Reading File ***************************************************/
+//	
+//	private ArrayList<Event> filterEventsToSpecificDate(String date, ArrayList<Event> eventBookByMonth) {
+//		ArrayList<Event> eventBookByDate = new ArrayList<Event>();
+//		
+//		for(Event anEvent : eventBookByMonth){
+//			if( anEvent.getStartDateString().toLowerCase().equals(date.toLowerCase())){
+//				eventBookByDate.add(anEvent);
+//			}
+//		}
+//		return eventBookByDate;
+//	}
+//	
+///********************************** Writing on File ***************************************************/
+//	
+//	private boolean saveEventBook(String textFileName, ArrayList<Event> eventBook){
+//		
+//		try{
+//			File outfile = new File(textFileName);
+//			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
+//			
+//			
+//			
+//			for(Event anEvent : eventBook){
+//				writer.write(anEvent.toString()); 
+//				writer.newLine();
+//			}
+//			writer.close();
+//			return true;
+//	
+//		}catch(IOException e){
+//			System.out.println("File cannot be written.\n");
+//			return false;
+//		}
+//	}
+//	
+//	private void sortEventsByDate(){
+//		
+//	}
 
 
 }
