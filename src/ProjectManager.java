89d121cc611cf8668df257767a2dbf2e689390c1
A0124402 <A0124402@u.nus.edu>
2015-09-19 20:52:30 +0800

    altered the ProjectManager deleteProjectEvent method

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -6,8 +6,8 @@ public interface ProjectManager {
 	public boolean createProject (String projectName);
 	
 	public boolean deleteProject(String projectName);
-	public boolean deleteProjectEvent(String eventName);
-	public boolean deleteProjectEvent(int index);
+	public boolean deleteProjectEvent(String eventName, String projectName);
+	public boolean deleteProjectEvent(int index, String projectName);
 	
 	public boolean editProjectEvent(int eventIndex, int infoIndex, String newValue);
 	public boolean editProjectEvent(String name, int infoIndex, String newValue);
