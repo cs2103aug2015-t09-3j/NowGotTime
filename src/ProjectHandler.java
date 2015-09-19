8888aef1108b5af00aeb67db9b1bdedac890d1d6
cloudiwind <cloudiwind@gmail.com>
2015-09-20 00:49:01 +0800

    ProjectHandler Methods and Test
    Test doesn't work

--- src/ProjectHandler.java
+++ src/ProjectHandler.java
@@ -3,59 +3,195 @@ import java.util.ArrayList;
 
 public class ProjectHandler implements ProjectManager{
 
+	private FileHandler project;
+	private ArrayList<Event> projectBook = new ArrayList<Event>();
+	private ArrayList<String> projectList = new ArrayList<String>();
+		
+	public ProjectHandler (){
+		project = new FileHandler();
+	}
+	
 	@Override
 	public boolean createProject(String projectName) {
+		//check: file name too long and blank file name
+
+		return project.createNewProject(projectName);
+	}
+
+	@Override
+	public boolean deleteProject(String projectName) {
+		
+		return project.deleteProject(projectName);
+	}
+
+	@Override
+	public boolean deleteProjectEvent(String eventName, String projectName) {
+		
+		for (Event anEvent : projectBook)
+		{
+			if (anEvent.getName().equals(eventName))
+			{
+				projectBook.remove(anEvent);
+				project.saveEditedProjectDetails(projectBook, projectName);
+				return true;
+			}
+		}
 		
 		return false;
 	}
 
 	@Override
-	public boolean deleteProject(String projectName) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean deleteProjectEvent(int index, String projectName) {
+		
+		if (index < projectBook.size())
+		{
+			projectBook.remove(index);
+			project.saveEditedProjectDetails(projectBook, projectName);
+			return true;
+		}
+		
+		else
+			return false;
 	}
 
 	@Override
-	public boolean deleteProjectEvent(String eventName) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean editProjectEvent(int eventIndex, int infoIndex, String newValue) {
+		
+		if (eventIndex <= 0 || eventIndex > projectBook.size())
+		{
+			return false;
+		}
 
-	@Override
-	public boolean deleteProjectEvent(int index) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean editProjectEvent(int eventIndex, int infoIndex,
-			String newValue) {
-		// TODO Auto-generated method stub
-		return false;
+		else
+		{
+			Event event = projectBook.get(eventIndex);
+			
+			switch(infoIndex)
+			{
+				case (1):
+				{
+					event.setName(newValue);
+					break;
+				}
+				
+				case (2):
+				{
+					event.updateStartDate(newValue); //Stef, for single date events, change both case 2 and 3
+					break;
+				}
+				
+				case (3):
+				{
+					event.updateEndDate(newValue);
+					break;
+				}
+				
+				case (4):
+				{
+					event.updateStartTime(newValue);
+					break;
+				}
+				
+				case (5):
+				{
+					event.updateEndTime(newValue);
+					break;
+				}
+				
+				case (6):
+				{
+					event.setAdditionalInfo(newValue);
+					break;
+				}
+			}
+			
+			return true;
+		}
 	}
 
 	@Override
 	public boolean editProjectEvent(String name, int infoIndex, String newValue) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		if (!projectBook.contains(name))
+		{
+			return false;
+		}
+
+		else
+		{
+			//Event event = projectBook.get(projectBook.indexOf(name));
+			
+			//recursion
+				int eventIndex = projectBook.indexOf(name);
+				return editProjectEvent(eventIndex, infoIndex, newValue);
+		}	
+		
+			//if recursion doesn't work:
+		
+			/*switch(infoIndex)
+			{
+				case (1):
+				{
+					event.setName(newValue);
+					break;
+				}
+				
+				case (2):
+				{
+					event.updateStartDate(newValue); //Stef, for single date events, change both case 2 and 3
+					break;
+				}
+				
+				case (3):
+				{
+					event.updateEndDate(newValue);
+					break;
+				}
+				
+				case (4):
+				{
+					event.updateStartTime(newValue);
+					break;
+				}
+				
+				case (5):
+				{
+					event.updateEndTime(newValue);
+					break;
+				}
+				
+				case (6):
+				{
+					event.setAdditionalInfo(newValue);
+					break;
+				}
+			}
+			
+			return true;
+		}*/
+			
 	}
 
 	@Override
-	public ArrayList<Event> viewProjectTimeline(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<String> listExistingProjects() {
+		
+		projectList = project.getListOfExistingProject();
+		return projectList;
+	}
+	
+	@Override
+	public ArrayList<Event> viewProjectTimeline(String projectName) {
+		
+		projectBook = project.retrieveProjectTimeLine(projectName);
+		return projectBook;
 	}
 
 	@Override
 	public ArrayList<Event> viewProjectTimeline(int index) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String projectName = projectList.get(index);
+		projectBook = project.retrieveProjectTimeLine(projectName);
+		return projectBook;
 	}
-
-	@Override
-	public ArrayList<String> listExistingProjects() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
---
+++ src/TestProject.java
@@ -0,0 +1,14 @@
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ProjectHandler test = new ProjectHandler();
+		
+		System.out.println(test.createProject("projectdiva"));
+		
+		//Dear RX, I cant do anything, if i can't create a project. SAVE ME PREASE
+	}
+
+}
