7e733332b2b5bd9d673c4cbe4c66e725d0399927
cloudiwind <cloudiwind@gmail.com>
2015-09-21 15:56:18 +0800

    Update Casual

--- src/TestProject.java
+++ src/TestProject.java
@@ -6,15 +6,17 @@ public class TestProject {
 		// Rmb to del file in main proj b4 running
 		
 		ProjectHandler test = new ProjectHandler();
-		Event pseudoEvent = new Event("eatpieday", "20 November 2015", "08:00", "18:00", "additional info la");
-		Event pseudoEvent2 = new Event("eatpieday2", "21 November 2015", "08:00", "18:00", "additional info la");
+		Event pseudoEvent = new Event("eatpieday", "20 Nov 2015", "08:00", "18:00", "i love pies");
+		Event pseudoEvent2 = new Event("eatpieday2", "21 Nov 2015", "08:00", "18:00", "i love pies a lot");
 		
 		System.out.println(test.createProject("projectdiva3"));
 		System.out.println(test.deleteProject("projectdiva3"));
 		System.out.println(test.createProject("projectdiva2"));
+		
 		//System.out.println(test.createProject("projectdiva2"));
 		System.out.println(test.addProjectEvent(pseudoEvent, "projectdiva2"));
 		System.out.println(test.addProjectEvent(pseudoEvent2, "projectdiva2"));
+		
 		//System.out.println(test.addProjectEvent(pseudoEvent, "projectdiva3"));
 		System.out.println(test.viewProjectTimeline("projectdiva2"));
 		System.out.println();
@@ -29,6 +31,9 @@ public class TestProject {
 		test.editProjectEvent(1, 5, "20:00", "projectdiva2");
 		test.editProjectEvent(1, 6, "no more info", "projectdiva2");
 		
+		/* toLower and toUpper case when doing comparison.
+		 * 
+		 */
 	}
 
 }
