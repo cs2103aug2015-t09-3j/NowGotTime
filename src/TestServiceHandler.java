98ef3e38cb730d084b0ef9ba2072f2137ac04d8c
JonathanSim <jszw12345@gmail.com>
2015-09-22 00:05:09 +0800

    waiting for event which takes in 4 string parameters

--- src/TestServiceHandler.java
+++ src/TestServiceHandler.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-
 public class TestServiceHandler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		ServiceHandler test = new ServiceHandler();
-		Event testEvent = new Event("study", "12 mar 2020", "08:00 ", "1800");
+		//Event testEvent = new Event("study", "12 mar 2020", "08:00 ", "1800");
 		
-		System.out.println(test.createEvent(testEvent));
+		//System.out.println(test.createEvent(testEvent));
 		System.out.println(test.viewEventByDate("12 March 2020"));
 		System.out.println(test.deleteEvent("study"));
 		
