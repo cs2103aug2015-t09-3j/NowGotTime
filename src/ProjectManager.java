6b1c628bd1a80b7dfcdbe97ce08ed535f29ff923
cloudiwind <cloudiwind@gmail.com>
2015-10-01 12:49:26 +0800

    Testing Update

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -3,19 +3,18 @@ import java.util.ArrayList;
 
 public interface ProjectManager {
 	
-	public boolean createProject (String projectName); 
+	public boolean createProject (String projectName); //works
 	
-	public boolean addProjectEvent (Event eventName, String projectName);
+	public boolean addProjectEvent (Event eventName, String projectName); //works
 	
-	public boolean deleteProject(String projectName); 
+	public boolean deleteProject(String projectName); //works
 	public boolean deleteProjectEvent(String eventName, String projectName);//not tested
 	public boolean deleteProjectEvent(int index, String projectName);//not tested
 	
-	public boolean editProjectEvent(int eventIndex, int infoIndex, String newValue, String projectName); 
-	public boolean editProjectEvent(Event eventName, int infoIndex, String newValue, String projectName);
-	//WHY DOES THIS NOT WORK. PROJECTBOOK.CONTAINS(EVENT) DOESNT WORK WHY //EVENT NEEDS TO EVENTNAME.GETNAME();?
+	public boolean editProjectEvent(int eventIndex, int infoIndex, String newValue, String projectName); //works
+	public boolean editProjectEvent(Event eventName, int infoIndex, String newValue, String projectName); //works
 	
-	public ArrayList<Event> viewProjectTimeline(String name);
-	public ArrayList<Event> viewProjectTimeline(int index);
-	public ArrayList<String> listExistingProjects();
+	public ArrayList<Event> viewProjectTimeline(String name); //works
+	public ArrayList<Event> viewProjectTimeline(int index); //works
+	public ArrayList<String> listExistingProjects(); //works
 	}
