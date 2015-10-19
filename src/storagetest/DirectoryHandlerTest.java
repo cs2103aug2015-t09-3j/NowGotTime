c4bcd7c101ffff133652d4f89fc1d3ddb40c5a2e
A0124402 <A0124402@u.nus.edu>
2015-10-20 01:18:29 +0800

    implement junit testing for directoryHandler

--- src/storagetest/DirectoryHandlerTest.java
+++ src/storagetest/DirectoryHandlerTest.java
@@ -2,6 +2,12 @@ package storagetest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,13 +28,25 @@ public class DirectoryHandlerTest {
 	public static void tearDownAfterClass() throws Exception {
 		System.out.println("Exiting, cleaning up folders");
 		if(PreparationCleanUp.cleanUp(newBaseDirectory)){
+			manualCleanUp();
 			System.out.println("Clean up completed. bye");
 		}
 	}
 
 	@Test
 	public void testDirectoryHandler() {
+		String baseDirectory = System.getProperty("user.dir").toString();
+		File file = new File(baseDirectory + "/Event");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
 		
+		file = new File(baseDirectory + "/Todo");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
+		
+		file = new File(baseDirectory + "/Project");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
 	}
 
 	@Test
@@ -50,7 +68,44 @@ public class DirectoryHandlerTest {
 		assertEquals("Test with valid new directory",
 				true, dirHand.setNewBaseDirectory(newBaseDirectory));
 		
+		String baseDirectory = System.getProperty("user.dir").toString() + "/alternateTestFiles";
+		File file = new File(baseDirectory + "/Event");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
+		
+		file = new File(baseDirectory + "/Todo");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
+		
+		file = new File(baseDirectory + "/Project");
+		assertEquals("Test with valid new directory",
+				true, file.exists());
 		
 	}
-
+	
+	private static void manualCleanUp(){
+		delete("Event");
+		delete("Todo");
+		delete("Project");
+		delete("overview.txt");		
+	}
+	
+	private static void delete(String name){
+		String baseDirectory = System.getProperty("user.dir").toString() + "/" + name;
+		File file = new File(baseDirectory);
+		if(file.isDirectory()){
+			Path path = Paths.get(baseDirectory);
+			
+			try {
+				Files.delete(path);
+			} catch(NoSuchFileException e) {
+				System.out.println("No such file exist to delete");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}else{
+			file.delete();
+		}
+	}
 }
