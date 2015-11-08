fde37cb963669c7a5c8b56215e8ecb81e4ce6dd8
cloudiwind <cloudiwind@gmail.com>
2015-11-08 11:45:55 +0800

    API

--- src/project/Projects.java
+++ src/project/Projects.java
@@ -22,6 +22,8 @@ public class Projects {
 	
 	/**
 	 * Adds a new Project into the ArrayList
+	 * @param projectName is the name of the Project to be created
+	 * @return returns true once Project is created, else return false
 	 */
 	public boolean createProject(String projectName) {
 		return project.createNewProject(projectName.toLowerCase().trim());
@@ -29,6 +31,8 @@ public class Projects {
 	
 	/**
 	 * Deletes an existing Project from the ArrayList
+	 * @param projectName is the name of the Project to be deleted
+	 * @return returns true once Project is deleted, else return false
 	 */
 	public boolean deleteProject(String projectName) {
 		return project.deleteProject(projectName.toLowerCase());
@@ -36,6 +40,9 @@ public class Projects {
 	
 	/**
 	 * Edits an existing Project from the ArrayList
+	 * @param newProjectName is the name of the Project that is edited to
+	 * @param oldProjectName is the name of the Project that is being edited
+	 * @return returns true once Project is edited, else return false.
 	 */
 	public boolean editProjectName(String newProjectName, String oldProjectName) {
 		/*if (createProject(newProjectName) && pHandler.editProjectName(newProjectName.toLowerCase(), oldProjectName.toLowerCase())
@@ -67,6 +74,7 @@ public class Projects {
 	
 	/**
 	 * Views an ArrayList of existing Projects
+	 * @return returns an ArrayList of Strings where each String is the name of existing Projects
 	 */
 	public ArrayList<String> listExistingProjects() {
 		return project.getListOfExistingProject();
@@ -74,6 +82,9 @@ public class Projects {
 	
 	/**
 	 * Adds an existing Event into a Project by the Project Name
+	 * @param event is the Event that is being added into the Project
+	 * @param projectName is the Project that the Event is added to
+	 * @return returns true once Event is added into Project, else return false
 	 */
 	public boolean addProjectEvent(Event event, String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -85,6 +96,9 @@ public class Projects {
 	
 	/**
 	 * Adds an existing Event into a Project by the Project Index
+	 * @param event is the Event that is being added into the Project
+	 * @param index is the index of the Project that the Event is added to
+	 * @return returns true once Event is added into Project, else return false
 	 */
 	public boolean addProjectEvent(Event event, int index) {
 		updateProjectList();
@@ -98,6 +112,9 @@ public class Projects {
 	
 	/**
 	 * Removes an Event added into a Project by Project Name and Event
+	 * @param event is the Event that is to be deleted from a Project
+	 * @param projectName is the name of the Project that is to be deleted from
+	 * @return returns true once the Event is deleted from the Project, else return false
 	 */
 	public boolean deleteProjectEvent(Event event, String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -109,6 +126,9 @@ public class Projects {
 	
 	/**
 	 * Removes an Event added into a Project by Project Index and Event
+	 * @param event is the Event that is to be deletd from a Project
+	 * @param index is the index of the Project that the Event is to be deleted from
+	 * @return returns true once the Event is deleted from the Project, else return false
 	 */
 	public boolean deleteProjectEvent(Event event, int index) {
 		updateProjectList();
@@ -129,6 +149,9 @@ public class Projects {
 	
 	/**
 	 * Removes an Event added into a Project by Project Name and Event Index
+	 * @param eventArrayListIndex is the index of the ArrayList that the Event is in
+	 * @param projectName is the name of the Project that the Event is being deleted from
+	 * @return returns true once Event is deleted from the Project, else return false
 	 */
 	public boolean deleteProjectEvent(int eventArrayListIndex, String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -140,6 +163,9 @@ public class Projects {
 	
 	/**
 	 * Extracts the Event to be edited by Project Name and Event Index
+	 * @param arrayIndex is the index of the ArrayList that holds the Event to be edited
+	 * @param projectName is the name of the Project that the Event is in
+	 * @return returns an Event that is to be edited, else return null 
 	 */
 	public Event editEvent(int arrayIndex, String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -151,6 +177,8 @@ public class Projects {
 	
 	/**
 	 * View an ArrayList of Event IDs by Project Name
+	 * @param projectName is the name of the Project that is being viewed
+	 * @return returns an ArrayList of Integers if Project exists, else return null
 	 */
 	public ArrayList<Integer> viewProjectTimeline(String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -162,6 +190,8 @@ public class Projects {
 	
 	/**
 	 * View an ArrayList of Event IDs by Project Index
+	 * @param index is the Index of the Project that is being viewed
+	 * @return returns an ArrayList of Integers if Project exists, else return null
 	 */
 	public ArrayList<Integer> viewProjectTimeline(int index) {
 		updateProjectList();
@@ -175,6 +205,8 @@ public class Projects {
 	
 	/**
 	 * View an ArrayList of Events by Project Name
+	 * @param projectName is the name of the Project that is being viewed
+	 * @return returns an ArrayList of Events if Project exists, else return null
 	 */
 	public ArrayList<Event> viewProjectTimelineInEvents(String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -186,6 +218,8 @@ public class Projects {
 	
 	/**
 	 * View an ArrayList of Events by Project Index
+	 * @param index is the Index of the Project that is being viewed
+	 * @return returns an ArrayList of Events if Project exists, else return null
 	 */
 	public ArrayList<Event> viewProjectTimelineInEvents(int index) {
 		updateProjectList();
@@ -197,30 +231,10 @@ public class Projects {
 		}
 	}
 	
-	
-	
-	/*
-	public boolean editProjectEvent(Event event, int infoIndex, String newValue, String projectName) {
-		if (!listExistingProjects().contains(projectName.toLowerCase())) {
-			return false;
-		} else {
-		return pHandler.editProjectEvent(event, infoIndex, newValue, projectName);
-		}
-	}
-	
-	public boolean editProjectEvent(Event event, int infoIndex, String newValue, int index) {
-		projectList = listExistingProjects();
-		if (index < projectList.size()) {
-			String projectName = projectList.get(index);
-			return editProjectEvent(event, infoIndex, newValue, projectName);
-		} else {
-			return false;
-		}
-	}
-	*/
-	
 	/**
 	 * Searches for a Project in an ArrayList of Projects
+	 * @param name is part of the name of the Project that is searched
+	 * @return returns an ArrayLst of Strings that contains Projects that contain the String name. 
 	 */
 	public ArrayList<String> searchProjects (String name) {
 		projectList = project.getListOfExistingProject();
@@ -235,6 +249,8 @@ public class Projects {
 	
 	/**
 	 * Calculates the percentage of completed Events in a Project
+	 * @param projectName is the Project name of the Project
+	 * @return returns a double value of the percentage of checked Events, else return negative value
 	 */
 	public double progressBar(String projectName) {
 		if (checkIfProjectExists(projectName)) {
@@ -245,6 +261,10 @@ public class Projects {
 	
 	/**
 	 * Adds a message into a HashMap of a Project
+	 * @param index is the index of the Event in a Project
+	 * @param projectName is the name of the Project that the message is being added to
+	 * @param progressMessage is the message that is being added to an Event
+	 * @return returns true once message is added to the Event, else return false
 	 */
 	public boolean addProgressMessage(int index, String projectName, String progressMessage) {
 		if (checkIfProjectExists(projectName)) {
@@ -255,6 +275,9 @@ public class Projects {
 	
 	/**
 	 * Deletes a message from a HashMap of a Project
+	 * @param index is the index of the Event in a Project
+	 * @param projectName is the name of the Project that the message is being deleted from
+	 * @return returns true once message is deleted from the Event, else return false
 	 */
 	public boolean deleteProgressMessage(int index, String projectName) {
 		if (checkIfProjectExists(projectName)) {
@@ -265,6 +288,10 @@ public class Projects {
 	
 	/**
 	 * Edits a message in a HashMap of a Project
+	 * @param index is the index of the Event in a Project
+	 * @param newProressMessage is the message that is being updated to 
+	 * @param projectNmae is the name of the Project that the new message is being added to
+	 * @return returns true once the message is edited in the Event, else return false
 	 */
 	public boolean editProgressMessage(int index, String newProgressMessage, String projectName) {
 		if (checkIfProjectExists(projectName)) {
@@ -275,6 +302,8 @@ public class Projects {
 
 	/**
 	 * Checks if Project exists in the ArrayList
+	 * @param projectName is the name of the Project that is being checked
+	 * @return returns true if the searched Project exists, else return false
 	 */
 	public boolean checkIfProjectExists(String projectName) {
 		return listExistingProjects().contains(projectName.toLowerCase());
@@ -282,6 +311,8 @@ public class Projects {
 	
 	/**
 	 * Views by Project Name, a Project Timeline with the Progress Message each Event holds
+	 * @param projectName is the name of the Project that is being viewed
+	 * @return returns an ArrayList of Events that holds the Events and its Progress Message
 	 */
 	public ArrayList<Event> viewEventProgressTimeline(String projectName) {
 		if (!checkIfProjectExists(projectName)) {
@@ -293,6 +324,8 @@ public class Projects {
 	
 	/**
 	 * Views by Project Index, a Project Timeline with the Progress Message each Event holds
+	 * @param index is the index of the Project that is being viewed
+	 * @return returns an ArrayList of Events that holds the Events and its Progress Message
 	 */
 	public ArrayList<Event> viewEventProgressTimeline(int index) {
 		updateProjectList();
@@ -306,6 +339,8 @@ public class Projects {
 	
 	/**
 	 * Searches for an Event in a Project
+	 * @param item is the item that is to be found 
+	 * @return returns a String that is the Project the item is found in, else return null
 	 */
 	public String searchItem(Item item) {
 	    int id = item.getId();
